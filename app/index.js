4350acba9da1452afa94563d0d5f80b1578bd608
Lenninlasd <lenninlasd@gmail.com>
2017-05-10 09:24:42 -0500

    Add webGL cube example

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import SVG from 'svg.js';
 import nj from 'numjs';
+import * as THREE from 'three';
 
 const matSize = [6,8];
 const features = nj.random(matSize);
@@ -10,8 +11,9 @@ const styles = {
     stroke: 'cyan',
     'stroke-width': 1
 }
-
 const draw = SVG('svg').size(matSize[1]*styles.size, matSize[0]*styles.size);
+
+createRow(features, styles);
 
 function rect(val, styles, position = {x: 0,  y: 0}) {
     const s = styles;
@@ -48,4 +50,36 @@ function createRow(features, styles) {
     }
 }
 
-createRow(features, styles);
+/******
+******
+******
+******/
+let camera, scene, renderer, mesh;
+
+init();
+animate();
+
+function init() {
+    const geometry = new THREE.BoxGeometry(200, 200, 200);
+    const material = new THREE.MeshBasicMaterial({
+        color: 0xff00ff,
+        wireframe: true
+    });
+
+    camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 1, 10000);
+    scene = new THREE.Scene();
+    renderer = new THREE.WebGLRenderer();
+    mesh = new THREE.Mesh(geometry, material);
+
+    camera.position.z = 500;
+    scene.add(mesh);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.02;
+    renderer.render(scene, camera);
+}
