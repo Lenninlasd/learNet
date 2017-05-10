c713e5059f1de72d36dc655653bd21378b1ab47f
Lenninlasd <lenninlasd@gmail.com>
2017-05-09 22:17:43 -0500

    Install babeljs

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,11 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+        ]
+    },
     plugins: [new HtmlWebpackPlugin(
         {template: './app/index.html'}
     )]
