b9cea1efe54f93b752b00c255a5edf6132e1dfbb
Lenninlasd <lenninlasd@gmail.com>
2017-05-12 15:48:37 -0500

    Run webgl_interactive_cubes_ortho

--- app/index.js
+++ app/index.js
@@ -1,85 +1,97 @@
-import SVG from 'svg.js';
-import nj from 'numjs';
 import * as THREE from 'three';
 
-const matSize = [6,8];
-const features = nj.random(matSize);
-const styles = {
-    size: 40,
-    fill: '#000',
-    fontFill: 'yellow',
-    stroke: 'cyan',
-    'stroke-width': 1
-}
-const draw = SVG('svg').size(matSize[1]*styles.size, matSize[0]*styles.size);
-
-createRow(features, styles);
-
-function rect(val, styles, position = {x: 0,  y: 0}) {
-    const s = styles;
-    const square = draw.rect(s.size, s.size)
-    .attr({
-        x: position.x,
-        y: position.y,
-        fill: s.fill,
-        stroke: s.stroke,
-        'stroke-width': s['stroke-width']
-    });
-    const textInfo = draw.text(val.toFixed(3).toString())
-    .attr({
-        x: position.x + s.size*0.2,
-        y: position.y + s.size*0.2
-    }).font({
-        size: s.size*0.25,
-        fill: s.fontFill
-    });
-    return [square, textInfo];
-}
-
-function createRow(features, styles) {
-    const shape = features.shape.length === 1 ?
-        [features.shape, 1] : features.shape;
-    const featuresArray = features.tolist();
-    for (let i = 0; i < shape[0]; i++) {
-        for (let j = 0; j < shape[1]; j++) {
-            let feature = features.get(i,j);
-            let x = j*styles.size;
-            let y = i*styles.size;
-            rect(feature, styles, {x: x,  y: y});
-        }
-    }
-}
-
-/******
-******
-******
-******/
-let camera, scene, renderer, mesh;
+let container, stats;
+let camera, scene, raycaster, renderer;
+let mouse = new THREE.Vector2(), INTERSECTED;
+let radius = 500, theta = 0;
+let frustumSize = 1000;
 
 init();
 animate();
 
 function init() {
-    const geometry = new THREE.BoxGeometry(200, 200, 200);
-    const material = new THREE.MeshBasicMaterial({
-        color: 0xff00ff,
-        wireframe: true
-    });
+    container = document.createElement( 'div' );
+    document.body.appendChild(container);
 
-    camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 1, 10000);
-    scene = new THREE.Scene();
-    renderer = new THREE.WebGLRenderer();
-    mesh = new THREE.Mesh(geometry, material);
+    let aspect = window.innerWidth / window.innerHeight;
+    camera = new THREE.OrthographicCamera(
+        frustumSize*aspect/- 2, frustumSize*aspect/2,
+        frustumSize/2, frustumSize/-2, 0.1, 10000
+    );
 
-    camera.position.z = 500;
-    scene.add(mesh);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+    // Scene and Light
+	scene = new THREE.Scene();
+	let light = new THREE.DirectionalLight(0xffffff, 1);
+	light.position.set(1, 1, 1).normalize();
+	scene.add( light );
+
+    // let geometry = new THREE.SphereBufferGeometry(20, 10, 10);
+    let geometry = new THREE.PlaneBufferGeometry(10, 10);
+	for ( let i = 0; i < 1000; i ++ ) {
+		let object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
+		object.position.x = Math.random() * 800 - 400;
+		object.position.y = Math.random() * 800 - 400;
+		object.position.z = Math.random() * 800 - 400;
+        let scale = Math.random() + 5;
+        object.scale.x = scale;
+		object.scale.y = scale;
+		object.scale.z = scale;
+
+        object.rotation.x = Math.random() * 2 * Math.PI;
+        object.rotation.y = Math.random() * 2 * Math.PI;
+        object.rotation.z = Math.random() * 2 * Math.PI;
+		scene.add(object);
+	}
+	raycaster = new THREE.Raycaster();
+	renderer = new THREE.WebGLRenderer();
+	renderer.setClearColor( 0xf0f0f0 );
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.sortObjects = false;
+	container.appendChild(renderer.domElement);
+
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+	window.addEventListener( 'resize', onWindowResize, false );
 }
-
+function onWindowResize() {
+	let aspect = window.innerWidth / window.innerHeight;
+	camera.left   = - frustumSize * aspect / 2;
+	camera.right  =   frustumSize * aspect / 2;
+	camera.top    =   frustumSize / 2;
+	camera.bottom = - frustumSize / 2;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
+function onDocumentMouseMove( event ) {
+	event.preventDefault();
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
+//
 function animate() {
-    requestAnimationFrame(animate);
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
-    renderer.render(scene, camera);
+	requestAnimationFrame( animate );
+	render();
+}
+function render() {
+    theta += 0.1;
+    camera.position.x = radius * Math.sin( THREE.Math.degToRad( theta ) );
+    camera.position.y = radius * Math.sin( THREE.Math.degToRad( theta ) );
+    camera.position.z = radius * Math.cos( THREE.Math.degToRad( theta ) );
+    camera.lookAt( scene.position );
+    camera.updateMatrixWorld();
+	// find intersections
+	raycaster.setFromCamera( mouse, camera );
+	let intersects = raycaster.intersectObjects( scene.children );
+	if ( intersects.length > 0 ) {
+		if ( INTERSECTED != intersects[ 0 ].object ) {
+            if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+                INTERSECTED = intersects[ 0 ].object;
+                INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+                INTERSECTED.material.emissive.setHex( 0xff0000 );
+		}
+	} else {
+		if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+		INTERSECTED = null;
+	}
+	renderer.render( scene, camera );
 }
