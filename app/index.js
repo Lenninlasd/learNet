cf625e823d2c60a2ff79fbdd00761338ed2069dc
Lenninlasd <lenninlasd@gmail.com>
2017-05-18 11:19:26 -0500

    Implement buffergeometry single scuare

--- app/index.js
+++ app/index.js
@@ -1,74 +1,97 @@
 import * as THREE from 'three';
 import nj from 'numjs';
-const t0 = performance.now();
-const matSize = [50,100];
-const features = nj.random(matSize);
-const style = {
-    size: 10,
-    fill: 0xffffff
-}
 
-let camera, scene, renderer;
+var container;
+var camera, scene, renderer;
+var mesh;
 
 init();
 animate();
-const t1 = performance.now();
-console.log(t1 - t0);
-
 function init() {
-    const aspect = window.innerWidth / window.innerHeight;
-    const frustumSize = 1000;
-    camera = new THREE.OrthographicCamera(
-        frustumSize*aspect/- 2, frustumSize*aspect/2,
-        frustumSize/2, frustumSize/-2, 0.1, 10000
-    );
-
-    // Scene and Light
+    container = document.getElementById( 'container' );
+    //
+    camera = new THREE.PerspectiveCamera( 27, window.innerWidth / window.innerHeight, 1, 3500 );
+    camera.position.z = 50;
     scene = new THREE.Scene();
-    const light = new THREE.DirectionalLight(0xffffff);
-    light.position.set(0, 0, 1).normalize();
-    scene.add(light);
 
-    // Create Geometry
-    createMatrix(features, style, scene);
+    //
+    scene.add( new THREE.AmbientLight( 0xffffff ) );
+    //
+    var triangles = 2;
+    var geometry = new THREE.BufferGeometry();
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.sortObjects = false;
+    // var vertices = new Float32Array(triangles * 4 * 3 );
+    var vertices = new Float32Array( [
+    	-1.0, -1.0,  1.0,
+    	 1.0, -1.0,  1.0,
+    	 1.0,  1.0,  1.0,
 
-    const container = document.createElement('div');
-    document.body.appendChild(container);
-    container.appendChild(renderer.domElement);
-}
+    	 1.0,  1.0,  1.0,
+    	-1.0,  1.0,  1.0,
+    	-1.0, -1.0,  1.0
+    ] );
 
-function rect(feature, geometry, style, position = {x: 0, y: 0}) {
-    const s = style;
-    const object = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
-        color: s.fill*feature
-    }));
-    object.position.x = position.x;
-    object.position.y = position.y;
-    return object;
-}
-
-function createMatrix(features, style, scene) {
-    const shape = features.shape.length === 1 ?
-        [features.shape, 1] : features.shape;
-    const featuresArray = features.tolist();
-    const geometry = new THREE.PlaneBufferGeometry(style.size, style.size);
-    for (let i = 0; i < shape[0]; i++) {
-        for (let j = 0; j < shape[1]; j++) {
-            let feature = featuresArray[i][j];
-            let x = j*style.size - window.innerWidth/2;
-            let y = window.innerHeight/2 - i*style.size;
-            scene.add(rect(feature, geometry, style, {x: x,  y: y}));
+    // for ( var i = 0, l = triangles * 4 * 3; i < l; i += 3 ) {
+    //     vertices[ i     ] = Math.random() - 0.5;
+    //     vertices[ i + 1 ] = Math.random() - 0.5;
+    //     vertices[ i + 2 ] = Math.random() - 0.5;
+	// }
+    geometry.addAttribute( 'position', new THREE.BufferAttribute( vertices, 3) );
+    let totalcolors = triangles * 3 * 3;
+    let totalScuareColor = totalcolors/2
+    var colors = new Uint8Array(totalcolors);
+    let red, green, blue;
+    for ( let i = 0; i < totalcolors; i += 9 ) {
+        // debugger;
+        if (( totalScuareColor * i ) % 18 === 0) {
+            red = Math.random(), green = Math.random(), blue = Math.random();
         }
-    }
-}
 
+        colors[ i     ] = red * 255;
+        colors[ i + 1 ] = green * 255;
+        colors[ i + 2 ] = blue * 255;
+
+        colors[ i + 3 ] = red * 255;
+        colors[ i + 4 ] = green * 255;
+        colors[ i + 5 ] = blue * 255;
+
+        colors[ i + 6 ] = red * 255;
+        colors[ i + 7 ] = green * 255;
+        colors[ i + 8 ] = blue * 255;
+    }
+    geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3, true ));
+
+    var material = new THREE.MeshPhongMaterial( {
+        color: 0xaaaaaa, specular: 0xffffff, shininess: 250,
+        side: THREE.DoubleSide, vertexColors: THREE.VertexColors,
+        transparent: true
+    } );
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+    //
+    renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor( 0x101010 );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.gammaInput = true;
+    renderer.gammaOutput = true;
+    container.appendChild( renderer.domElement );
+
+    window.addEventListener( 'resize', onWindowResize, false );
+}
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+//
 function animate() {
-    requestAnimationFrame(animate);
-    camera.position.z = 1000;
-    renderer.render(scene, camera);
+    requestAnimationFrame( animate );
+    render();
+}
+function render() {
+    var time = Date.now() * 0.001;
+    mesh.rotation.x = time * 0.25;
+    mesh.rotation.y = time * 0.5;
+    renderer.render( scene, camera );
 }
