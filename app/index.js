19ef28b8417af8c3383fe55756ce5823af701f79
Lenninlasd <lenninlasd@gmail.com>
2017-05-24 19:48:05 -0500

    add LineFormation

--- app/index.js
+++ app/index.js
@@ -1,3 +1,4 @@
+// https://codepen.io/cx20/pen/rcDbl
 import * as THREE from 'three';
 import nj from 'numjs';
 import TWEEN from 'tween.js';
@@ -43,16 +44,22 @@ function init() {
 
     //The smaller the first number is, the closer the cube appears
     camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1500);
-    //The height of the camera in comparison to the scene
     camera.position.x = 0;
     camera.position.y = 0;
-    //The zoom level of the camera
     camera.position.z = 1200;
 
     //Create the scene
     scene = new THREE.Scene();
     group = new THREE.Object3D();
     scene.add(group);
+
+    var light = new THREE.DirectionalLight(0xffffff, 1.5);
+    light.position.set(1, 1, 1).normalize();
+    scene.add(light);
+
+    var light2 = new THREE.DirectionalLight(0xffffff);
+    light2.position.set(-1, -1, -1).normalize();
+    scene.add(light2);
 
     var i, j;
     var x, y, z;
@@ -74,49 +81,22 @@ function init() {
                 meshArray[i].position.x = x - 0;
                 meshArray[i].position.y = y;
                 meshArray[i].position.z = z;
-                //scene.add(meshArray[i]);
                 group.add(meshArray[i]);
                 list.push(meshArray[i]);
             }
         }
     }
 
-    var light = new THREE.DirectionalLight(0xffffff, 1.5);
-    light.position.set(1, 1, 1).normalize();
-    scene.add(light);
-
-    var light2 = new THREE.DirectionalLight(0xffffff);
-    light2.position.set(-1, -1, -1).normalize();
-    scene.add(light2);
 
     // create and start the renderer; choose antialias setting.
-    if (isWebgl()) {
-        renderer = new THREE.WebGLRenderer({
-            antialias: true
-        });
-    }else {
-        renderer = new THREE.CanvasRenderer();
-    }
+    renderer = new THREE.WebGLRenderer({antialias: true});
 
     renderer.setSize(window.innerWidth - 5, window.innerHeight - 5);
     container.appendChild(renderer.domElement);
 
-    // document.addEventListener('mousedown', onDocumentMouseDown, false);
-    // document.addEventListener('touchstart', onDocumentTouchStart, false);
-    // document.addEventListener('touchmove', onDocumentTouchMove, false);
-
     window.addEventListener('resize', onWindowResize, false);
 
-    for (i = 0; i < list.length; i++) {
-        new TWEEN.Tween(list[i].scale).to({
-            x: 1,
-            y: 1,
-            z: 1
-        }, 1000)
-            .easing(TWEEN.Easing.Back.Out).start();
-    }
-
-    setInterval(changeID, 3000);
+    setInterval(changeID, 2000);
 }
 
 function onWindowResize() {
@@ -127,53 +107,6 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     render();
 }
-
-// function onDocumentMouseDown(event) {
-//     event.preventDefault();
-//
-//     document.addEventListener('mousemove', onDocumentMouseMove, false);
-//     document.addEventListener('mouseup', onDocumentMouseUp, false);
-//     document.addEventListener('mouseout', onDocumentMouseOut, false);
-//
-//     mouseXOnMouseDown = event.clientX - windowHalfX;
-//     targetRotationOnMouseDown = targetRotation;
-// }
-//
-// function onDocumentMouseMove(event) {
-//     mouseX = event.clientX - windowHalfX;
-//     mouseY = event.clientY - windowHalfY;
-// }
-//
-// function onDocumentMouseUp(event) {
-//     document.removeEventListener('mousemove', onDocumentMouseMove, false);
-//     document.removeEventListener('mouseup', onDocumentMouseUp, false);
-//     document.removeEventListener('mouseout', onDocumentMouseOut, false);
-// }
-
-// function onDocumentMouseOut(event) {
-//     //To get rid of redundant code, call function that does same thing above
-//     onDocumentMouseUp(event);
-// }
-
-// function onDocumentTouchStart(event) {
-//     if (event.touches.length === 1) {
-//         event.preventDefault();
-//
-//         mouseXOnMouseDown = event.touches[0].pageX - windowHalfX;
-//         targetRotationOnMouseDown = targetRotation;
-//     }
-// }
-
-// function onDocumentTouchMove(event) {
-//     if (event.touches.length === 1) {
-//         event.preventDefault();
-//
-//         mouseX = event.touches[0].pageX - windowHalfX;
-//         //The rotation of the cube on its' x-axis
-//         targetRotation = targetRotationOnMouseDown + (mouseX - mouseXOnMouseDown) * rotationSpeed;
-//     }
-// }
-
 
 /**************THIS BLOCK IS NECESSARY -> DRAWS DEBUG AXES********************/
 var debugaxis = function (axisLength) {
@@ -199,18 +132,10 @@ var debugaxis = function (axisLength) {
     createAxis(v(0, -axisLength, 0), v(0, axisLength, 0), 0x00FF00);
     createAxis(v(0, 0, -axisLength), v(0, 0, axisLength), 0x0000FF);
 };
-
 //To use enter the axis length
 //debugaxis(400);
 /**************************************************************************************/
 
-function isWebgl() {
-    try {
-        return !!window.WebGLRenderingContext && !! document.createElement('canvas').getContext('experimental-webgl');
-    } catch (e) {
-        return false;
-    }
-}
 
 function animate() {
     requestAnimationFrame(animate);
@@ -228,24 +153,31 @@ function render() {
 }
 
 function changeID() {
-
     switch (ID) {
     case 1:
+        console.log('Random changeFormation1');
         changeFormation1();
         break;
     case 2:
+        console.log('Cube');
         changeFormation2();
         break;
     case 3:
+        console.log('Spiral');
         changeFormation3();
         break;
+    case 4:
+        console.log('LineFormation');
+        LineFormation();
+        break;
     default:
+        console.log('Random 2');
         changeFormation1();
         break;
     }
 
     ID++;
-    if (ID > 3) {
+    if (ID > 4) {
         ID = 1;
     }
 }
@@ -262,15 +194,27 @@ function changeFormation1() {
             x: vx,
             y: vy,
             z: vz
-        }, 1000)
-            .easing(TWEEN.Easing.Exponential.InOut).start();
+        }, 1000).easing(TWEEN.Easing.Exponential.InOut).start();
 
         new TWEEN.Tween(list[i].rotation).to({
             x: 0,
             y: rot,
             z: 0
-        }, 1000)
-            .easing(TWEEN.Easing.Cubic.InOut).start();
+        }, 1000).easing(TWEEN.Easing.Cubic.InOut).start();
+    }
+}
+
+// Line
+function LineFormation() {
+    for (var i = 0; i < list.length; i++) {
+        new TWEEN.Tween(list[i].position).to({
+            x: i * DOT_SIZE + X_START_POS - 1200,
+            y: 0,
+            z: 0
+        }, 1000).easing(TWEEN.Easing.Exponential.InOut).start();
+        list[i].rotation.x = 0;
+        list[i].rotation.y = 0;
+        list[i].rotation.z = 0;
     }
 }
 
