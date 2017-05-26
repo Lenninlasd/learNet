d010255914f415c88f44fcef47bc343cad54299c
Lenninlasd <lenninlasd@gmail.com>
2017-05-26 10:23:44 -0500

    Add Transposed matrix animation

--- app/index.js
+++ app/index.js
@@ -1,14 +1,17 @@
 import * as THREE from 'three';
 import nj from 'numjs';
+import TWEEN from 'tween.js';
+
 const t0 = performance.now();
-const matSize = [50,100];
+const matSize = [12,6];
 const features = nj.random(matSize);
 const style = {
-    size: 10,
+    size: 50,
     fill: 0xffffff
 }
-
+debugger;
 let camera, scene, renderer;
+let group, list;
 
 init();
 animate();
@@ -25,12 +28,15 @@ function init() {
 
     // Scene and Light
     scene = new THREE.Scene();
+    group = new THREE.Object3D();
+    scene.add(group);
+
     const light = new THREE.DirectionalLight(0xffffff);
     light.position.set(0, 0, 1).normalize();
     scene.add(light);
 
     // Create Geometry
-    createMatrix(features, style, scene);
+    list = createMatrix(features, style, group);
 
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -40,6 +46,15 @@ function init() {
     const container = document.createElement('div');
     document.body.appendChild(container);
     container.appendChild(renderer.domElement);
+
+    setTimeout(transposed, 3000);
+}
+
+function animate() {
+    TWEEN.update();
+    requestAnimationFrame(animate);
+    camera.position.z = 1000;
+    renderer.render(scene, camera);
 }
 
 function rect(feature, geometry, style, position = {x: 0, y: 0}) {
@@ -52,23 +67,68 @@ function rect(feature, geometry, style, position = {x: 0, y: 0}) {
     return object;
 }
 
-function createMatrix(features, style, scene) {
+function createMatrix(features, style, group) {
     const shape = features.shape.length === 1 ?
         [features.shape, 1] : features.shape;
     const featuresArray = features.tolist();
     const geometry = new THREE.PlaneBufferGeometry(style.size, style.size);
+    const list = [];
     for (let i = 0; i < shape[0]; i++) {
         for (let j = 0; j < shape[1]; j++) {
             let feature = featuresArray[i][j];
             let x = j*style.size - window.innerWidth/2;
             let y = window.innerHeight/2 - i*style.size;
-            scene.add(rect(feature, geometry, style, {x: x,  y: y}));
+            let rectangle = rect(feature, geometry, style, {x: x,  y: y});
+            group.add(rectangle);
+            list.push(rectangle);
+        }
+    }
+    return list;
+}
+
+function xLineFormation() {
+    for (var i = 0; i < list.length; i++) {
+        new TWEEN.Tween(list[i].position).to({
+            x: i * style.size - window.innerWidth/2,
+            y: window.innerHeight/2,
+            z: 0
+        }, 4000).easing(TWEEN.Easing.Exponential.InOut).start();
+    }
+}
+function yLineFormation() {
+    for (var i = 0; i < list.length; i++) {
+        new TWEEN.Tween(list[i].position).to({
+            x: - window.innerWidth/2,
+            y: window.innerHeight/2 - i * style.size,
+            z: 0
+        }, 10000).easing(TWEEN.Easing.Exponential.InOut).start();
+    }
+}
+
+function transposed() {
+    let k = 0;
+    for (let i = 0; i < matSize[0]; i++) {
+        for (let j = 0; j < matSize[1]; j++) {
+            new TWEEN.Tween(list[k].position).to({
+                x: i * style.size - window.innerWidth/2,
+                y: window.innerHeight/2 - j * style.size,
+                z: 0
+            }, 2000).easing(TWEEN.Easing.Exponential.InOut).start();
+            k++;
         }
     }
 }
 
-function animate() {
-    requestAnimationFrame(animate);
-    camera.position.z = 1000;
-    renderer.render(scene, camera);
+function rotate() {
+    let k = 0;
+    for (let i = 0; i < matSize[0]; i++) {
+        for (let j = matSize[1]; j > 0; j--) {
+            new TWEEN.Tween(list[k].position).to({
+                x: i * style.size - window.innerWidth/2,
+                y: window.innerHeight/2 - j * style.size,
+                z: 0
+            }, 3000).easing(TWEEN.Easing.Exponential.InOut).start();
+            k++;
+        }
+    }
 }
