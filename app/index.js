a36a803be9e308f05bbc1b275e7338a84b3bb53a
Lenninlasd <lenninlasd@gmail.com>
2017-05-26 14:30:00 -0500

    Encapsulate and create multiple objects

--- app/index.js
+++ app/index.js
@@ -1,15 +1,12 @@
 import * as THREE from 'three';
 import nj from 'numjs';
 import TWEEN from 'tween.js';
+import MatrixGL from './matrix';
 
 const t0 = performance.now();
-const matSize = [12,6];
+const matSize = [60,30];
 const features = nj.random(matSize);
-const style = {
-    size: 50,
-    fill: 0xffffff
-}
-debugger;
+
 let camera, scene, renderer;
 let group, list;
 
@@ -19,12 +16,10 @@ const t1 = performance.now();
 console.log(t1 - t0);
 
 function init() {
-    const aspect = window.innerWidth / window.innerHeight;
-    const frustumSize = 1000;
+    // OrthographicCamera( left, right, top, bottom, near, far )
     camera = new THREE.OrthographicCamera(
-        frustumSize*aspect/- 2, frustumSize*aspect/2,
-        frustumSize/2, frustumSize/-2, 0.1, 10000
-    );
+        -window.innerWidth*1.1/2, window.innerWidth/2,
+        window.innerHeight*1.1/2, -window.innerHeight/2, 1, 1000 );
 
     // Scene and Light
     scene = new THREE.Scene();
@@ -35,9 +30,6 @@ function init() {
     light.position.set(0, 0, 1).normalize();
     scene.add(light);
 
-    // Create Geometry
-    list = createMatrix(features, style, group);
-
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -47,88 +39,26 @@ function init() {
     document.body.appendChild(container);
     container.appendChild(renderer.domElement);
 
-    setTimeout(transposed, 3000);
+    // Create Geometry
+    const mtrx = MatrixGL(features, group, {
+        size: 9,
+        fill: 0xffffff,
+        startPoint: [window.innerWidth/2, window.innerHeight/2]
+    });
+
+    const mtrx2 = MatrixGL(features, group, {
+        size: 5,
+        fill: 0xffffff,
+        startPoint: [window.innerWidth/2, 0]
+    });
+
+    setTimeout(mtrx.transposed, 1500);
+    setTimeout(mtrx2.transposed, 2000);
 }
 
 function animate() {
     TWEEN.update();
     requestAnimationFrame(animate);
-    camera.position.z = 1000;
+    camera.position.z = 10;
     renderer.render(scene, camera);
-}
-
-function rect(feature, geometry, style, position = {x: 0, y: 0}) {
-    const s = style;
-    const object = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
-        color: s.fill*feature
-    }));
-    object.position.x = position.x;
-    object.position.y = position.y;
-    return object;
-}
-
-function createMatrix(features, style, group) {
-    const shape = features.shape.length === 1 ?
-        [features.shape, 1] : features.shape;
-    const featuresArray = features.tolist();
-    const geometry = new THREE.PlaneBufferGeometry(style.size, style.size);
-    const list = [];
-    for (let i = 0; i < shape[0]; i++) {
-        for (let j = 0; j < shape[1]; j++) {
-            let feature = featuresArray[i][j];
-            let x = j*style.size - window.innerWidth/2;
-            let y = window.innerHeight/2 - i*style.size;
-            let rectangle = rect(feature, geometry, style, {x: x,  y: y});
-            group.add(rectangle);
-            list.push(rectangle);
-        }
-    }
-    return list;
-}
-
-function xLineFormation() {
-    for (var i = 0; i < list.length; i++) {
-        new TWEEN.Tween(list[i].position).to({
-            x: i * style.size - window.innerWidth/2,
-            y: window.innerHeight/2,
-            z: 0
-        }, 4000).easing(TWEEN.Easing.Exponential.InOut).start();
-    }
-}
-function yLineFormation() {
-    for (var i = 0; i < list.length; i++) {
-        new TWEEN.Tween(list[i].position).to({
-            x: - window.innerWidth/2,
-            y: window.innerHeight/2 - i * style.size,
-            z: 0
-        }, 10000).easing(TWEEN.Easing.Exponential.InOut).start();
-    }
-}
-
-function transposed() {
-    let k = 0;
-    for (let i = 0; i < matSize[0]; i++) {
-        for (let j = 0; j < matSize[1]; j++) {
-            new TWEEN.Tween(list[k].position).to({
-                x: i * style.size - window.innerWidth/2,
-                y: window.innerHeight/2 - j * style.size,
-                z: 0
-            }, 2000).easing(TWEEN.Easing.Exponential.InOut).start();
-            k++;
-        }
-    }
-}
-
-function rotate() {
-    let k = 0;
-    for (let i = 0; i < matSize[0]; i++) {
-        for (let j = matSize[1]; j > 0; j--) {
-            new TWEEN.Tween(list[k].position).to({
-                x: i * style.size - window.innerWidth/2,
-                y: window.innerHeight/2 - j * style.size,
-                z: 0
-            }, 3000).easing(TWEEN.Easing.Exponential.InOut).start();
-            k++;
-        }
-    }
 }
---
+++ app/matrix.js
@@ -0,0 +1,88 @@
+import * as THREE from 'three';
+import TWEEN from 'tween.js';
+
+
+export default function MatrixGL(features, group, style) {
+    const params = createMatrix(features, group, style);
+
+    return {
+        transposed:  function () {
+            transposed(params);
+        }
+    };
+}
+
+function createMatrix(features, group, style) {
+    const shape = features.shape.length === 1 ? [features.shape, 1] : features.shape;
+    const featuresArray = features.tolist();
+    const geometry = new THREE.PlaneBufferGeometry(style.size, style.size);
+    const list = [];
+    for (let i = 0; i < shape[0]; i++) {
+        for (let j = 0; j < shape[1]; j++) {
+            let feature = featuresArray[i][j];
+            let x = j*style.size - style.startPoint[0];
+            let y = style.startPoint[1] - i*style.size;
+            let rectangle = rect(feature, geometry, style, {x: x,  y: y});
+            group.add(rectangle);
+            list.push(rectangle);
+        }
+    }
+    return { list, shape, style };
+}
+
+function rect(feature, geometry, style, position = {x: 0, y: 0}) {
+    const s = style;
+    const object = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
+        color: s.fill*feature
+    }));
+    object.position.x = position.x;
+    object.position.y = position.y;
+    return object;
+}
+
+function xLineFormation() {
+    for (var i = 0; i < list.length; i++) {
+        new TWEEN.Tween(list[i].position).to({
+            x: i * style.size - window.innerWidth/2,
+            y: window.innerHeight/2,
+            z: 0
+        }, 4000).easing(TWEEN.Easing.Exponential.InOut).start();
+    }
+}
+function yLineFormation() {
+    for (var i = 0; i < list.length; i++) {
+        new TWEEN.Tween(list[i].position).to({
+            x: - window.innerWidth/2,
+            y: window.innerHeight/2 - i * style.size,
+            z: 0
+        }, 10000).easing(TWEEN.Easing.Exponential.InOut).start();
+    }
+}
+
+function rotate(params) {
+    let k = 0;
+    for (let i = 0; i < params.shape[0]; i++) {
+        for (let j = params.shape[1]; j > 0; j--) {
+            new TWEEN.Tween(params.list[k].position).to({
+                x: i * params.style.size - window.innerWidth/2,
+                y: window.innerHeight/2 - j * params.style.size,
+                z: 0
+            }, 3000).easing(TWEEN.Easing.Exponential.InOut).start();
+            k++;
+        }
+    }
+}
+
+function transposed(params) {
+    let k = 0;
+    for (let i = 0; i < params.shape[0]; i++) {
+        for (let j = 0; j < params.shape[1]; j++) {
+            new TWEEN.Tween(params.list[k].position).to({
+                x: i * params.style.size - params.style.startPoint[0],
+                y: params.style.startPoint[1] - j * params.style.size,
+                z: 0
+            }, 2000).easing(TWEEN.Easing.Exponential.InOut).start();
+            k++;
+        }
+    }
+}
